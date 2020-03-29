19c50b733930067af2e9ef2626c8147d630f3d05
Jon Dearden <jon.dearden@goworks.com>
2020-03-28 22:36:54 -0400

    Now actually including the code snippet

--- breathe/app/src/main/java/ottawa/ventilator/CodeFromBrian.java
+++ breathe/app/src/main/java/ottawa/ventilator/CodeFromBrian.java
@@ -1,4 +1,33 @@
 package ottawa.ventilator;
 
 public class CodeFromBrian {
+
+    /*
+        if one of you has an arduino, upload this script to it
+        it will help you test serial
+        i can make it so it sends a serial command back too
+
+
+        void setup() {
+          Serial.begin(9600);
+        }
+
+        String readSerial(){
+          char commandString[20]={};
+          uint8_t i=0;
+          while(Serial.available()>0){
+            commandString[i] = Serial.read();
+            i++;
+          }
+          return commandString;
+        }
+
+        void loop() {
+          String command = readSerial();
+          Serial.print(command);
+
+        }
+
+     */
 }
+
